51fcd07e5d203ba89123522fb948088f886e6cb9
ClownsharkBatwing <clownshark.batwing@proton.me>
2025-05-12 08:44:30 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,19 +1,23 @@
 import importlib
 
 modules_to_import = [
-    'UltraCascade.nodes',
-    'UltraCascade.nodes_sag_rag',
-    'UltraCascade.nodes_stage_b_patcher'
+    "custom_nodes.UltraCascade.nodes",
+    "custom_nodes.UltraCascade.nodes_sag_rag",
+    "custom_nodes.UltraCascade.nodes_stage_b_patcher",
 ]
 
 NODE_CLASS_MAPPINGS = {}
 NODE_DISPLAY_NAME_MAPPINGS = {}
 
 for module_name in modules_to_import:
-    module = importlib.import_module(module_name)
-    
-    if hasattr(module, 'NODE_CLASS_MAPPINGS'):
+    try:
+        module = importlib.import_module(module_name)
+        print(f"[UltraCascade] Imported {module_name}")
+    except Exception as e:
+        print(f"[UltraCascade] Failed to import {module_name}: {e}")
+        continue
+
+    if hasattr(module, "NODE_CLASS_MAPPINGS"):
         NODE_CLASS_MAPPINGS.update(module.NODE_CLASS_MAPPINGS)
-    
-    if hasattr(module, 'NODE_DISPLAY_NAME_MAPPINGS'):
+    if hasattr(module, "NODE_DISPLAY_NAME_MAPPINGS"):
         NODE_DISPLAY_NAME_MAPPINGS.update(module.NODE_DISPLAY_NAME_MAPPINGS)
